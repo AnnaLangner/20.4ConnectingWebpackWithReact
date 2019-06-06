647bc059935224ae38cb8e3722ab1de0907d5cbd
Ania <annalangner06@gmail.com>
2019-06-06 14:23:46 +0200

    Configuration Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,2 +1,9 @@
 const path = require('path');
 
+module.exports = {
+    entry: './src/index.js',
+    output: {
+        path: path.resolve(__dirname, 'build'),
+        filename: 'app.bundle.js'
+    },
+};
