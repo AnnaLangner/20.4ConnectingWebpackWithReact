e297619e3c39928b47f48c67f90467c27c546ee7
Ania <annalangner06@gmail.com>
2019-06-07 09:55:48 +0200

    Adding style-loader and css-loader

--- webpack.config.js
+++ webpack.config.js
@@ -6,4 +6,24 @@ module.exports = {
         path: path.resolve(__dirname, 'build'),
         filename: 'app.bundle.js'
     },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.css$/,
+                use: [
+                    {loader: 'style-loader'},
+                    {
+                        loader: 'css-loader',
+                        options: {
+                            modules: true
+                        }
+                    }
+                ]
+            }
+        ]
+    }
 };
